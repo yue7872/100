1d890906864f43ba8ec3e932dfb0b0dea748b144
yue7872 <2258462872@qq.com>
2022-04-25 13:34:44 +0800

    feat:优化生成的文案 。

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,18 +1,21 @@
 <script setup lang="ts">
-const countArr = Array.from({ length: 100 }, (_, idx) => idx.toString().padStart(3, '0'))
+import { info } from '../info/title'
+const countArr = computed(() => {
+  return Array.from({ length: info.length }, (_, idx) => idx.toString().padStart(3, '0'))
+})
 </script>
 
 <template>
   <div w-500px flex flex-wrap m-auto>
     <a
-      v-for="i
-        in countArr"
-      :key="i"
-      :href="`./challenge/${i}.vue`"
-      class="w-1/3"
+      v-for="(count,idx) in countArr"
+      :key="idx"
+      :href="`./challenge/${count}.vue`"
+      flex-1
+      class="min-w-1/3"
     >
-      <span>{{ i }}</span>
-      {{ i }}
+      <span>{{ count }}</span>
+      {{ info[idx].title }}
     </a>
   </div>
 </template>
